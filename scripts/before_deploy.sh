8b4d1cbeca83562c7af95783b155198e7d0932c9
hajooy <115938530+hajooy@users.noreply.github.com>
2023-01-09 17:09:36 +0900

    Update before_deploy.sh

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#cd /backup
-#rm -rf ROOT
+cd /backup
+rm -rf scriptes/ deploy.war appspec.yml
 cd /usr/local/tomcat/webapps
-mv -f appspec.yml deploy.war scripts/ /backup/
+mv -f appspec.yml ROOT/ scripts/ /backup/
